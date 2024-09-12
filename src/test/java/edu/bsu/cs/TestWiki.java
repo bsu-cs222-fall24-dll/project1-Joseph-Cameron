d838355b377a4e7c1fa32b1e681e9f86adc3b6ee
joseph-sobiech <joseph.sobiech@bsu.edu>
2024-09-12 18:19:07 -0400

    Added Classes, PickUserPage progressed

--- src/test/java/edu/bsu/cs/TestWiki.java
+++ src/test/java/edu/bsu/cs/TestWiki.java
@@ -1,4 +1,41 @@
 package edu.bsu.cs;
 
+
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Objects;
+
+;
+
 public class TestWiki {
-}
+
+        @Test
+        public void testAccessToJsonFile() throws IOException {
+            String jsonData = readSampleFileAsString();
+            Assertions.assertNotNull(jsonData);
+        }
+
+        @Test
+        public void testCountRevisionsWithJsonPath() throws IOException {
+            String jsonData = readSampleFileAsString();
+            JSONArray revisions = getRevisionsFromJson(jsonData);
+            Assertions.assertEquals(15, revisions.size());
+        }
+
+        private String readSampleFileAsString() throws NullPointerException, IOException {
+            try (InputStream sampleFile = Thread.currentThread().getContextClassLoader()
+                    .getResourceAsStream("sample.json")) {
+                return new String(Objects.requireNonNull(sampleFile).readAllBytes(), Charset.defaultCharset());
+            }
+        }
+
+        private JSONArray getRevisionsFromJson(String jsonData) {
+            return JsonPath.read(jsonData, "$..revisions[*]");
+        }
+    }
