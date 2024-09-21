77c56b39d97717c711ce23f6c282b20f22858b26
joseph-sobiech <joseph.sobiech@bsu.edu>
2024-09-21 16:53:20 -0400

    Changed Append Message and i less than value

--- src/main/java/edu/bsu/cs/ParseWikiInfo.java
+++ src/main/java/edu/bsu/cs/ParseWikiInfo.java
@@ -5,17 +5,14 @@ import net.minidev.json.JSONArray;
 
 public class ParseWikiInfo extends PickUserPage{
 
-
     public String parseUserAndTimestamp(String jsonData){
         //Creates string with resultFromRevisions using StringBuilder
         StringBuilder resultFromRevisions = new StringBuilder();
-        for(int i = 1; i < 15; i++) {
-            JSONArray userValue = JsonPath.read(jsonData, "$..revisions["+ i +"].user");
+        for (int i = 1; i < 16; i++) {
+            JSONArray userValue = JsonPath.read(jsonData, "$..revisions[" + i + "].user");
             JSONArray timestampValue = JsonPath.read(jsonData, "$..revisions[" + i + "].timestamp");
-            resultFromRevisions.append("User: ").append(userValue.get(0));
-            resultFromRevisions.append(", Timestamp: ").append(timestampValue.get(0)).append("\n");
-
-
+            resultFromRevisions.append("  ").append(timestampValue.get(0)).append("\n");
+            resultFromRevisions.append("  ").append(userValue.get(0));
         }
         return resultFromRevisions.toString();
     }
