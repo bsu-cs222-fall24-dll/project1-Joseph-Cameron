8fca52e0a7e2345dad157b6ec7e4a99ce7a12e9f
joseph-sobiech <joseph.sobiech@bsu.edu>
2024-09-26 10:02:01 -0400

    Added input and output Fields to UI

--- src/main/java/edu/bsu/cs/UI.java
+++ src/main/java/edu/bsu/cs/UI.java
@@ -1,5 +1,44 @@
 package edu.bsu.cs;
 
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+
+import static com.jayway.jsonpath.internal.PathRef.createRoot;
+import static javafx.application.Application.launch;
+
 public class UI {
-    
-}
+    public static void main(String[] args) {
+        launch(args);
+    }
+    public ParseWikiInfo parseWikiInfo = new ParseWikiInfo();
+    static final Button wikiButton = new Button("Enter Wikipedia Name");
+    public TextField inputField = new TextField();
+    public TextField outPutField = new TextField();
+
+    @Override
+    public void start(Stage primaryStage) {
+        outPutField.setEditable(false);
+        configure(primaryStage);
+    }
+
+    private void configure(Stage stage) {
+        stage.setTitle("Get Info");
+        stage.setScene(new Scene(createRoot()));
+        stage.sizeToScene();
+        stage.show();
+    }
+
+    private Pane createRoot() {
+        Vbox root= new Vbox();
+        root.getChildren().add(inputField);
+        root.getChildren().add();
+        root.getChildren().add(outPutField);
+        return root;
+    }
+
+}//end class UI
