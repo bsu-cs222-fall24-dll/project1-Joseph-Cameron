f6f8f51e60d82281a64b27a946fc16ec9f52346a
joseph-sobiech <joseph.sobiech@bsu.edu>
2024-09-26 09:14:59 -0400

    Changed Gradle Build, added UI class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
     id("application")
+    id ("org.openjfx.javafxplugin") version "0.0.13"
 }
 
-group = "org.example"
+group = "edu.bsu.cs"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -11,13 +12,23 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.slf4j:slf4j-nop:2.0.11")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+    implementation ("net.minidev:json-smart:2.5.0")
     implementation("com.jayway.jsonpath:json-path:2.9.0")
-    implementation("net.minidev:json-smart:2.5.0")
+    implementation("org.slf4j:slf4j-nop:2.0.11")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+javafx {
+    version = "22"
+    modules("javafx.controls", "javafx.fxml")
+}
+application {
+    mainClass.set("edu.bsu.cs.Main")
 }
